8d4dabb976843c1ae64ad53a5f39228a6ec18cfa
viniciusoike <viniciusoike@gmail.com>
2025-07-28 15:25:21 -0300

    finish day 12 submission

--- R/12_datagov.R
+++ R/12_datagov.R
@@ -1,10 +1,15 @@
-library(tidyverse)
-library(sf)
-library(osmdata)
+library(dplyr)
+library(patchwork)
+library(ggplot2)
+library(ragg)
+import::from(stringr, str_extract)
+import::from(readr, read_csv)
 
-dat <- read_csv("//Volumes/T7 Touch/bases-de-dados/real-estate/Real_Estate_Sales_2001-2022_GL.csv")
+dat <- read_csv(
+  "//Volumes/T7 Touch/bases-de-dados/real-estate/Real_Estate_Sales_2001-2022_GL.csv"
+)
 
-dat <- dat |>
+subdat <- dat |>
   janitor::clean_names() |>
   filter(!is.na(location)) |>
   mutate(
@@ -13,66 +18,152 @@ dat <- dat |>
     lat = as.numeric(str_extract(location, "(?<=[0-9] )[0-9].+(?=\\))"))
   )
 
-cities <- dat |>
+subdat <- subdat |>
+  filter(
+    !(property_type %in% c("Commercial", "Industrial", "Vacant Land"))
+  ) |>
+  filter(!is.na(property_type)) |>
+  filter(sale_amount > 25000, sale_amount < 1e7)
+
+main_cities <- subdat |>
   count(town, sort = TRUE) |>
-  head(6) |>
+  head(5) |>
   pull(town)
 
-dat |>
-  filter(town %in% cities) |>
-  ggplot(aes(lng, lat)) +
-  geom_point() +
-  facet_wrap(vars(town), scales = "free")
+main_cities <- c(main_cities, "Hartford")
 
+subdat |>
+  count(property_type, residential_type)
 
-bbox <- getbb(str_glue("{cities[1]}, Connecticut, United States"))
-
-place <- osmdata::opq(bbox)
-
-border <- add_osm_feature(place, key = "boundary", value = "administrative")
-border <- osmdata_sf(border)
-
-border <- border$osm_multipolygons |>
-  filter(border_type == "town", name == cities[1])
-
-subdat <- dat |>
-  filter(list_year >= 2021, town == cities[1]) |>
-  st_as_sf(coords = c("lng", "lat"), crs = 4326)
-
-
-get_streets <- function(place, border) {
-
-  #> Importa todas as principais vias da cidade
-  streets <- osmdata::add_osm_feature(
-    place,
-    key = "highway",
-    value = c("motorway", "trunk", "primary", "secondary", "tertiary", "residential")
+subdat <- subdat |>
+  mutate(
+    house_type = case_when(
+      property_type == "Apartments" ~ "Apartment",
+      TRUE ~ residential_type
+    ),
+    property_type = "Residential",
+    residential_type = if_else(
+      is.na(residential_type),
+      "Apartments",
+      residential_type
+    )
   )
 
-  #> Converte o dado
+subdat <- subdat |>
+  filter(town %in% main_cities) |>
+  mutate(
+    date = as.Date(date_recorded, format = "%m/%d/%Y"),
+    year = lubridate::year(date),
+    month = lubridate::month(date)
+  )
 
-  streets <- osmdata::osmdata_sf(streets)
-  streets <- osmdata::unique_osmdata(streets)
-  streets <- streets[["osm_lines"]]
-  streets <- dplyr::select(streets, osm_id, name, highway)
+# subdat |>
+#   filter(town == "Waterbury") |>
+#   summarise(
+#     avg = mean(sale_amount),
+#     med = median(sale_amount),
+#     .by = c("year", "property_type", "residential_type")
+#   ) |>
+#   View()
 
-  #> Enconrtra a intersecção entre as estradas e o limites do município
-  streets_border <- sf::st_intersection(streets, border)
-  #> Retorna o objeto streets_border
-  return(streets_border)
+series <- subdat |>
+  filter(house_type %in% c("Single Family", "Two Family", "Condo")) |>
+  summarise(
+    count = n(),
+    avg = mean(sale_amount),
+    med = median(sale_amount),
+    .by = c("year", "month", "town")
+  ) |>
+  mutate(date = lubridate::make_date(year, month, 1))
 
-}
+min(series$year)
+max(series$year)
 
-streets <- get_streets(place, border)
+series <- series |>
+  arrange(date) |>
+  arrange(town)
 
-factorial(24)/(factorial(12) * factorial(12)) * (3/7)^13 * (4/7)^12
+series <- series |>
+  mutate(
+    adjusted_price = priceR::adjust_for_inflation(
+      med,
+      from_date = date,
+      to_date = as.Date("2022-01-01"),
+      country = "US"
+    )
+  )
 
-st_make_grid(border, cellsize = )
+main_year_breaks <- c(2007, seq(2010, 2020, 5), 2023)
+year_breaks <- 2007:2023
+dbreaks <- lubridate::make_date(year_breaks)
 
-dnbinom(prob = 3/7, size = 13, x = 25 - 13)
+offwhite <- "#fefefe"
+pal_col_axis <- c(offwhite, "#000000")
+col_axis_inds <- as.integer(year_breaks %in% main_year_breaks) + 1
+col_axis <- pal_col_axis[col_axis_inds]
+size_axis <- c(0, 12)[col_axis_inds]
+
+pal_colors <- MetBrewer::met.brewer("Hokusai1", n = 6)
+pal_colors[4] <- MetBrewer::met.brewer("Hokusai1", n = 24)[5]
+pal_colors[2] <- MetBrewer::met.brewer("Hokusai1", n = 24)[15]
+
+font_text <- "DIN Alternate"
+font_title <- "Rockwell"
+
+base_plot <- ggplot(series, aes(date, adjusted_price / 1e3, color = town)) +
+  geom_point(alpha = 0.15, shape = 21, aes(fill = town)) +
+  geom_smooth(method = "gam", se = FALSE) +
+  geom_hline(yintercept = 0, lwd = 1) +
+  geom_label(
+    data = filter(series, date == max(date), .by = "town"),
+    aes(x = date, y = adjusted_price / 1e3, label = town),
+    size = 3,
+    family = font_text,
+    hjust = 0,
+    nudge_x = 100,
+    nudge_y = c(0, 0, -40, 40, 0, 0)
+  ) +
+  guides(color = "none", fill = "none") +
+  scale_x_date(
+    breaks = dbreaks,
+    date_label = "%Y",
+    expand = expansion(c(0.025, 0.2))
+  ) +
+  scale_y_continuous(
+    breaks = seq(0, 700, by = 100),
+    limits = c(NA, 700),
+    expand = expansion(c(0, 0.05))
+  ) +
+  scale_color_manual(name = "", values = pal_colors) +
+  scale_fill_manual(name = "", values = pal_colors) +
+  labs(x = NULL, y = "Median House Price ($ thous.)") +
+  theme_minimal(base_family = font_text) +
+  theme()
 
 
-ggplot() +
-  geom_sf(data = subdat, alpha = 0.5, size = 0.1) +
-  geom_sf(data = border, fill = NA) +
-  geom_sf(data = streets, linewidth = 0.4)
+theme_plot <- theme_minimal(base_size = 10, base_family = font_text) +
+  theme(
+    panel.background = element_rect(fill = offwhite, color = offwhite),
+    plot.background = element_rect(fill = offwhite, color = offwhite),
+    panel.grid.minor = element_blank(),
+    axis.text.x = element_text(size = size_axis),
+    axis.ticks.x = element_line(color = "gray20")
+  )
+
+theme_annotation <- theme(
+  plot.title = element_text(
+    size = 18,
+    color = "#000000",
+    family = font_title
+  ),
+  plot.subtitle = element_text(size = 11, color = "gray30"),
+  plot.caption = element_text(hjust = 0, color = "gray40")
+)
+
+base_plot +
+  plot_annotation(
+    title = "House Prices in Connecticut",
+    subtitle = "Median monthly house sale price in constant 2022 USD (thousands) of main cities in Conneticut.\nSample inclues only Single and Two Family Homes, and Condo units in the 25k-10M range.",
+    caption = "Source: Real Estate Sales 2001-2022 GL (data.gov). Smooth line is a GAM trend.\nNote: data has missing values.",
+    theme = theme_annotation
+  )
